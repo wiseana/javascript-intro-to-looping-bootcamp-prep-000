ecdf3b3834493f87f0b46215aa3773cc335f290c
Anastasia Wise <wise.anastasia@gmail.com>
2018-06-09 14:54:40 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -6,5 +6,8 @@ function forLoop(array) {
 function whileLoop(number) {
   while(number>0) {console.log(--number)}; 
   return 'done';}
- function doWhileLoop(array) {function maybeTrue() {return Math.random()>=0.5} do {array.length ---1} while (array.length>0 && maybeTrue()); return array}
+ function doWhileLoop(array) {
+   function maybeTrue() {return Math.random()>=0.5} 
+   do {array.length ---1} while (array.length>0 && maybeTrue()); 
+   return array}
  
