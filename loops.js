69dc7720c232ae518e5755b19e84ef5c5eaa6432
Anastasia Wise <wise.anastasia@gmail.com>
2018-06-09 14:26:22 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -3,4 +3,6 @@ function forLoop(array) {
     if (i===1) {array.push ("I am 1 strange loop.")} 
     else {array.push ("I am "+i+" strange loops.")}}
     return array;}
-    
+function whileLoop(number) {
+  while(number>0) {console.log(--number)}; 
+  if (number===0) {console.log("done")};}
